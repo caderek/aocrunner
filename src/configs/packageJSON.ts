d25d183b4ffb325d619013cbeb98614992462a57
Maciej Caderek <maciej.caderek@gmail.com>
2021-12-03 19:58:53 +0100

    Updated badges

--- src/configs/packageJSON.ts
+++ src/configs/packageJSON.ts
@@ -18,7 +18,7 @@ const packageJSON = ({ year, language, author }: Setup) => {
     license: "ISC",
     devDependencies: {
       "@types/node": "^16.11.6",
-      aocrunner: "^1.4.10",
+      aocrunner: "^1.4.11",
       prettier: "^2.4.1",
     },
     dependencies: {},
--- src/configs/readmeMD.ts
+++ src/configs/readmeMD.ts
@@ -89,7 +89,7 @@ const readmeMD = (
     <!-- Entries between SOLUTIONS and RESULTS tags are auto-generated -->
 
     [![AoC](https://badgen.net/badge/AoC/${year}/blue)](https://adventofcode.com/${year})
-    [![Node](https://badgen.net/badge/Node/v16.0.0+/blue)](https://nodejs.org/en/download/)
+    [![Node](https://badgen.net/badge/Node/v16.13.0+/blue)](https://nodejs.org/en/download/)
     ![Language](https://badgen.net/badge/Language/${lang}/blue)
     [![Template](https://badgen.net/badge/Template/aocrunner/blue)](https://github.com/caderek/aocrunner)
 
